ed5faadb7ce4f3a8fedea8f77247e05c7d8ddcf7
mastan511 <mastanvali.apssdc@gmail.com>
2020-03-23 11:57:05 +0530

    level3practicecommit

---
+++ Packages/myfun.py
@@ -0,0 +1,12 @@
+marks = 100
+def predict(x):
+    return (x*2)+1
+
+
+def properName(f,l):
+    a = f.strip().capitalize()
+    b = l.strip().capitalize()
+    return a+" "+b
+
+def proper(name):
+    return name.title().strip()
